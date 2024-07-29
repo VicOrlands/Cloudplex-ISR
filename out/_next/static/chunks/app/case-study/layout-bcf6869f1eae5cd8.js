7f3a3466b6dc3f02ef056e16a7c2b52aef08da59
Chimezie Innocent <chimezieinnocent39@gmail.com>
2024-07-29 03:34:27 +0100

    build: build files to export out/ folder

---
+++ out/_next/static/chunks/app/case-study/layout-bcf6869f1eae5cd8.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4576,2768,1246,4967,7359],{63521:function(){}},function(n){n.O(0,[2971,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
--- out/_next/static/chunks/app/events/layout-bba9e33c9e5a8051.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4967,1246,2768,4576,7359],{63521:function(){}},function(n){n.O(0,[2971,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
