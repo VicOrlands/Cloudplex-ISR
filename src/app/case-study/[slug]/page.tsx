f81f601d80c1c3eeb933484bfbd269498a5dc0f1
Rahma yaqeen <famirapt@gmail.com>
2024-12-06 13:18:28 +0100

    feat: moved entirely to cms (blog & case studies)\n fixed testimonial play issue

--- src/app/case-study/[slug]/page.tsx
+++ src/app/case-study/[slug]/page.tsx
@@ -12,41 +12,65 @@ type SlugParams = {
 
 export async function generateStaticParams() {
     const res = await fetch('https://bw5bt69rjh.execute-api.af-south-1.amazonaws.com/prod/case-studies/');
-    const data = await res.json();
-
-    const publishedCaseStudies = data.filter((caseStudy: { published: boolean }) => caseStudy.published);
-    const publishedSlug = publishedCaseStudies.map((pub: { slug: string; }) => ({ slug: pub.slug }));
-
-    const hardcodedSlugs = paths.map(({ url }) => ({ slug: url }));
-
-    return [
-        ...publishedSlug,
-        ...hardcodedSlugs,
-        { slug: "empowering-climate-decision-making-with-aws-iot-for-climdes" }
-    ];
-}
-
-export async function generateMetadata({ params }: SlugParams): Promise<Metadata> {
-    const res = await fetch(`https://bw5bt69rjh.execute-api.af-south-1.amazonaws.com/prod/case-studies/${params.slug}`);
-    const data = await res.json();
 
     if (res.ok){
-        const publishedSlug = data
+
+        const data = await res.json();
     
+        const publishedCaseStudies = data.filter((caseStudy: { published: boolean }) => caseStudy.published);
+        const publishedSlug = publishedCaseStudies.map((pub: { slug: string; }) => ({ slug: pub.slug }));
     
-        if (publishedSlug?.published) {
-            return {
-                title: `${publishedSlug.title} - CloudPlexo's Expert Solutions`,
-                description: publishedSlug.description,
-                alternates: {
-                    canonical: `https://cloudplexo.com/case-study/${publishedSlug.slug}`,
-                },
-            };
-        }
+        const hardcodedSlugs = paths.map(({ url }) => ({ slug: url }));
+    
+        return [
+            ...publishedSlug,
+            ...hardcodedSlugs,
+            { slug: "empowering-climate-decision-making-with-aws-iot-for-climdes" }
+        ];
     }
 
+    return []
 }
 
+export async function generateMetadata({
+    params,
+  }: SlugParams): Promise<Metadata> {
+    try {
+      const res = await fetch(
+        `https://bw5bt69rjh.execute-api.af-south-1.amazonaws.com/prod/case-studies/${params.slug}`
+      );
+  
+      if (res.ok) {
+        const data = await res.json();
+  
+        if (data?.published) {
+          return {
+            title: `${data.title} - CloudPlexo's Expert Solutions`,
+            description: data.description,
+            alternates: {
+              canonical: `https://cloudplexo.com/case-study/${data.slug}`,
+            },
+          };
+        }
+      }
+  
+      // Fallback metadata for non-OK responses or unpublished case studies
+      return {
+        title: 'Case Study Not Found - CloudPlexo',
+        description: 'The requested case study could not be found or is not published.',
+      };
+    } catch (error) {
+      console.error('Error fetching metadata:', error);
+  
+      // Fallback metadata in case of an error
+      return {
+        title: 'Error - CloudPlexo',
+        description: 'An error occurred while fetching the case study metadata.',
+      };
+    }
+  }
+
+  
 export default async function CaseStudyPage({ params }: { params: { slug: string } }) {
     const res = await fetch(`https://bw5bt69rjh.execute-api.af-south-1.amazonaws.com/prod/case-studies/${params.slug}`);
     if (res.ok){
